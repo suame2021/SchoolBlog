7df16b0f3b20e8ff799e2f7fb422ab90252676f3
suame2021 <85235483+suame2021@users.noreply.github.com>
2022-09-19 01:36:17 +0100

    nn

--- myblog/src/carouselComponent.js
+++ myblog/src/carouselComponent.js
@@ -4,12 +4,12 @@ import Slider2 from './images/Slider2.jpg'
 import Slider3 from "./images/Slider3.jpg"
 
 const CarouselComponent = () => {
-    return ( 
-        <Carousel className="sliderHeight">
+    return (
+      <Carousel className="sliderHeight">
         <Carousel.Item className="sliderHeight">
           <img
             className="d-block w-100"
-            src={slider1}
+            src=".Images/slider1.jpg"
             alt="First slide"
           />
           <Carousel.Caption>
@@ -18,31 +18,21 @@ const CarouselComponent = () => {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item className="sliderHeight">
-          <img
-            className="d-block w-100"
-            src={Slider3}
-            alt="Second slide"
-          />
-  
+          <img className="d-block w-100" src={Slider3} alt="Second slide" />
+
           <Carousel.Caption>
             <h3 className="sliderText">Our Students</h3>
-            
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item className="sliderHeight">
-          <img
-            className="d-block w-100"
-            src={Slider2}
-            alt="Third slide"
-          />
-  
+          <img className="d-block w-100" src={Slider2} alt="Third slide" />
+
           <Carousel.Caption>
             <h3 className="sliderText">Our Campus</h3>
-           
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-     );
+    );
 }
  
 export default CarouselComponent;
